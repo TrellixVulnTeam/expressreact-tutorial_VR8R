2ff8a00e33b8b556297e9c7850209f3faa62d4c8
Robyn Drake <robin.drake.041@gmail.com>
2022-05-23 03:30:46 -0500

    models/index.js and models/todo.js done

---
+++ src/todo-backend/models/todo.js
@@ -0,0 +1,17 @@
+const mongoose = reqquire("mongoose");
+
+const todoSchema = new mongoose.Schema({
+	// creating a schema for todo
+	task: {
+	  	// field1: task
+	  	type: String, // task is a string
+	  	unique: true, // it has to be unique
+	  	required: true // it is required
+	},
+	completed: {
+	  	// field2: completed
+	  	type: Boolean, // it is a boolean
+	 	default: false, // the default is false
+	}
+	
+});
